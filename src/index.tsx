890f235684c0078ad4263c207847d290661124c1
Rafa <wirecobweb@gmail.com>
2019-10-31 22:15:44 +0100

    redux, reducer, ducks

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Router} from 'react-router';
 import createHistory from 'history/createBrowserHistory';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import * reducers from './ducks';
 
-
+const store = createStore(combineReducers({
+    ...reducers,
+}));
 const history = createHistory();
 
 ReactDOM.render(
-<Router history={history}><App /></Router>,
+    <Provider store={store}>
+        <Router history={history}>
+            <App /></Router>
+    </Provider>
+,
  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
