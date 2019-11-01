0d916e20ff2e51c333b8c7132fc85d3bf50efe07
Rafa <wirecobweb@gmail.com>
2019-11-01 14:11:11 +0100

    in the absence of refactoring and changing the css

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,11 +1,20 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    
+    apiKey: "?????",
+    authDomain: "?????",
+    databaseURL: "?????",
+    projectId: "?????",
+    storageBucket: "?????",
+    messagingSenderId: "?????",
+    appId: "?????"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  const firestore = firebase.firestore();
+  const settings = {timestampsInSnapshots: true}
+  firestore.settings(settings)
   export const auth = firebase.auth();
-  export const db = firebase.firestore();
+  export const db = firestore
   export const storage = firebase.storage();
